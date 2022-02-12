68daadc63b85659f3976333e51647e2e0c93c8f2
ManfredBalvet <manfred.balvet@gmail.com>
2022-02-12 17:33:20 -0500

    All car data list without regex

--- car_data.py
+++
@@ -1,7 +0,0 @@
-class CarData:
-    def __init__(self, manufacturer, model, year, version):
-        self.manufacturer = manufacturer
-        self.model = model
-        self.year = year
-        self.version = version
-
